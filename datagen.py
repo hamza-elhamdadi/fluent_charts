fc774930ea0ba053aa40a6af4764e782a7c4fe5e
Hamza Elhamdadi <helhamdadi@umass.edu>
2025-02-11 12:31:20 -0500

    generated data

--- datagen.py
+++ datagen.py
@@ -5,10 +5,10 @@ n = 100
 groups = [{"name": "A", "mean": 1}, {"name": "B", "mean": 3}]
 data ={"nodes": []}
 
-for i, g in enumerate(groups, 1):
+for i, g in enumerate(groups):
     points = random.normal(loc=g["mean"], scale=0.3, size=(n,))
     for j, p in enumerate(points, 1):
-        data["nodes"].append({"group": g["name"], "index": i*j})
+        data["nodes"].append({"group": g["name"], "index": i*n + j})
 
 with open("data/al.json", "w") as file:
     json.dump(data, file, indent=4)
