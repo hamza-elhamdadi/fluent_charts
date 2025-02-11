a91b056cc0b64d49666c50b59055c281735803a9
Hamza Elhamdadi <helhamdadi@umass.edu>
2025-02-11 16:30:51 -0500

    beeswarm plot, trying to add mean rules

--- datagen.py
+++ datagen.py
@@ -8,7 +8,7 @@ data ={"nodes": []}
 for i, g in enumerate(groups):
     points = random.normal(loc=g["mean"], scale=0.3, size=(n,))
     for j, p in enumerate(points, 1):
-        data["nodes"].append({"group": g["name"], "index": i*n + j})
+        data["nodes"].append({"group": g["name"], "value": p, "index": i*n + j})
 
 with open("data/al.json", "w") as file:
     json.dump(data, file, indent=4)
